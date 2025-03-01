3f34ac629f12389fb25aa6aae3914fe1459ffd6c
jamarama <153025639+jamarama@users.noreply.github.com>
2025-02-28 18:23:33 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <><div className='main-nav'>
+      <h1 className="text-center">Astrological Trivia</h1>
+      <p className="text-center">How well do you know the star signs!</p>
+    </div><div className='content'>
+        <p>Cards go Here</p>
+      </div></>
+  );
+}
+
+export default App;
