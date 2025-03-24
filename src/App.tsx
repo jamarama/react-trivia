488421479f4d95120f1a9eb4a4231f07c755c9aa
jamarama <153025639+jamarama@users.noreply.github.com>
2025-03-24 09:25:56 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+
+function App() {
+  return (
+    <><div className='main-nav'>
+      <h1 className="text-center">Astrological Trivia</h1>
+      <p className="text-center">How well do you know the signs!</p>
+    </div><div className='content'>
+      <div className='card'></div>
+      
+    </div></>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// PascalCasing
+function Message()  {
+    // JSX: JavaScript XML
+    const name = 'Nia';
+    return <h1>Hello {name}</h1>;
+}
+export default Message;
