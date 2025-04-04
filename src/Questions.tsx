9b933b4917c7fc8550ec6a9384a3374d1ac98e34
jamarama <153025639+jamarama@users.noreply.github.com>
2025-04-04 10:53:03 -0500

    trivia

--- src/Questions.tsx
+++ src/Questions.tsx
@@ -1,20 +1,24 @@
-function Questions({category, title, description}) {
+type QuestionProps = {
+  category: string;
+  question: string;
+  answer: string;
+  options: string[];
+  onAnswer: (isCorrect: boolean) => void;
+};
+
+function Questions({ question, options, answer, onAnswer }: QuestionProps) {
   return (
-   
-    <div className="card" >
-
-        <div className="top">
-            <p>{category}</p>
-        </div>
-
-
-        <div className="bottom">
-            <h2>{title}</h2>
-            <p>{description}</p>
-        </div>
+    <div className="card">
+      <div className="bottom">
+        <h2>{question}</h2>
+        {options.map((option, index) => (
+          <button key={index} onClick={() => onAnswer(option === answer)}>
+            {option}
+          </button>
+        ))}
+      </div>
     </div>
-
   );
 }
 
-export default Questions
+export default Questions;
